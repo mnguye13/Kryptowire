92bd2a43a165355d97e00e9399bb7fc4954d3e0a
Minh Nguyen <mnguye13@hawk.iit.edu>
2020-02-11 02:58:28 -0600

    Add mutation for cars

--- Backend/api/models/cars.js
+++ Backend/api/models/cars.js
@@ -14,11 +14,36 @@ const cars = [
   }
 ];
 
-const typeDefs = `type Query {cars: [Car]}
-type Car {brand: String, model: String}`;
+const typeDefs = `
+type Query 
+{
+    info: String,
+    cars: [Car]
+}
+type Car {
+    brand: String, 
+    model: String
+}
+type Mutation{
+    addCar(brand: String, model: String): Car
+}
+
+`;
 
 const resolvers = {
-  Query: { cars: () => cars }
+  Query: {
+    info: () => "This is the api for Cars",
+    cars: () => cars
+  },
+  Mutation: {
+    addCar: (parent, args) => {
+      const car = {
+        brand: args.brand,
+        model: args.model
+      };
+      return car;
+    }
+  }
 };
 
 module.exports = Cars = makeExecutableSchema({
